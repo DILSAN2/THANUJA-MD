f16c37ae3566cbe995e05e3638ed10dd5778f5ae
DILSAN2 <dilsanthanuja2011@gmail.com>
2025-06-26 18:16:10 +0530

    Create creds.js

---
+++ auth_info_baileys/creds.js
@@ -0,0 +1,30 @@
+{
+  "noiseKey": {
+    "private": "...",
+    "public": "..."
+  },
+  "signedIdentityKey": {
+    "private": "...",
+    "public": "..."
+  },
+  "signedPreKey": {
+    "keyId": 1,
+    "private": "...",
+    "public": "..."
+  },
+  "registrationId": 123456789,
+  "advSecretKey": "...",
+  "signalIdentities": [
+    {
+      "identifier": {
+        "name": "94702509233",
+        "deviceId": 0
+      },
+      "identifierKey": "..."
+    }
+  ],
+  "lastAccountSyncTimestamp": 0,
+  "accountSettings": {
+    "unarchiveChats": true
+  }
+      }
